61b7f8f0a76a5ac66a9f7d5b71877c55ebb11855
Mathew Cheung <mathewcheung@mathews-mbp>
2018-12-26 20:34:07 -0600

    Connected blank boxes to IBActions in viewController

--- First Game/ViewController.swift
+++ First Game/ViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    //Press the boxes and then it will give choice of "X" or "O"
+    @IBAction func BlankBoxOne(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func BlankBoxTwo(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func BlankBoxThree(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func BlankBoxFour(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func BlankBoxFive(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func BlankBoxSix(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func BlankBoxSeven(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func BlankBoxEight(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func BlankBoxNine(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
