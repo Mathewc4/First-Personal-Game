a6a4f8a6482480da4b6bf57501e01cfa933b77be
Mathew Cheung <mathewcheung@mathews-mbp>
2018-12-26 19:44:25 -0600

    Initial Commit

---
+++ First Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First Game
+//
+//  Created by Mathew Cheung on 2018-12-26.
+//  Copyright © 2018 Mathew Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ First GameTests/First_GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  First_GameTests.swift
+//  First GameTests
+//
+//  Created by Mathew Cheung on 2018-12-26.
+//  Copyright © 2018 Mathew Cheung. All rights reserved.
+//
+
+import XCTest
+@testable import First_Game
+
+class First_GameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
